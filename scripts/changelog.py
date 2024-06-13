ff6960de4a83142b3d99771b7cb367e0c3924a49
$GITHUB_ACTOR <github-action@users.noreply.github.com>
2024-06-13 11:28:00 -0400

    update changelog script

--- scripts/changelog.py
+++ scripts/changelog.py
@@ -5,6 +5,7 @@ This script outputs a changelog markdown to stdout.
 """
 
 import subprocess
+import re
 
 # Get all the tags for the project
 git_tag_result = subprocess.run(
@@ -13,6 +14,20 @@ git_tag_result = subprocess.run(
 
 # Make a tag list
 tags = git_tag_result.stdout.split("\n")
+
+# Remove v from tags
+for i in range(len(tags)):
+    tags[i] = tags[i].replace('v','')
+
+# Remove empty strings from tags
+tags[:] = [x for x in tags if x]
+
+# Sort tags by semver number
+tags.sort(key = lambda x: [int(y) for y in x.split('.')])
+
+# Add v back into tags
+for i in range(len(tags)):
+    tags[i] = 'v'+tags[i]
 
 # Iterate through all the tags in the tag list and get a list of commits
 count = 0
@@ -29,8 +44,22 @@ for tag in tags:
             git_log_result = subprocess.getoutput(
                 f'git log --pretty=oneline {tag}...{prev_tag} | grep -v Merge | cut -d " " -f 2-'
             )
+        
         # Convert the commit list to a set and then back to a list to remove any duplicate commits.
         git_logs = list(set(git_log_result.split('\n')))
+        
+        # Sort git_logs so they are in the same order each time 
+        git_logs.sort()
+        
+        # Add links to pull requests
+        for i in range(len(git_logs)):
+            if re.search(r'\(\#\d*\)',git_logs[i]):
+                test = re.search(r'\(\#\d*\)',git_logs[i])
+                num = test.group()
+                num = num[2:]
+                num = num[:-1]
+                sub = ' https://github.com/itential/nickAtest/pull/' + num
+                git_logs[i] = re.sub('\(\#\d*\)',sub,git_logs[i])
 
         # Add the commits to the changelogs dictionary using the tag as the key and the 
         # commit list as the value
@@ -55,3 +84,9 @@ for release,changes in reversed(changelogs.items()):
     for change in changes:
         print(f'* {change}')
     print()
+
+    for i in range(len(tags)):
+        if i > 0:
+            if release == tags[i]:
+                full = 'https://github.com/itential/nickAtest/compare/' + tags[i-1] + '...' + release
+                print('Full Changelog:', full, '\n\n')
